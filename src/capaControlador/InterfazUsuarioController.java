32617dd417388db1ccecf6ebc09d564e0629e466
pinedas <pinedas@spineda-laptop.ter.teradyne.com>
2015-04-15 23:39:40 -0600

    Se agregar tabla de doctores a la interfaz de usuario.

--- src/capaControlador/InterfazUsuarioController.java
+++ src/capaControlador/InterfazUsuarioController.java
@@ -6,8 +6,17 @@
 package capaControlador;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.collections.ObservableList;
+import capaLogica.*;
 
 /**
  * FXML Controller class
@@ -15,13 +24,37 @@ import javafx.fxml.Initializable;
  * @author pinedas
  */
 public class InterfazUsuarioController implements Initializable {
+	
+	@FXML private TableView<Doctor> tablaDoctores;
+    @FXML private TextField idDoctorField;
+    @FXML private TextField nombreDoctorField;
+    @FXML private TextField especialidadField;
+    @FXML private TextField telefonoField;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    	ObservableList<Doctor> data = tablaDoctores.getItems();
+    	List<Doctor> lista = (new MultiDoctor()).buscarTodos();
+    	
+    	data.setAll(lista);
     }    
+    
+    @FXML
+    protected void addDoctor(ActionEvent event) {
+        ObservableList<Doctor> data = tablaDoctores.getItems();
+        data.add(new Doctor(idDoctorField.getText(),
+        	nombreDoctorField.getText(),
+        	especialidadField.getText(),
+        	telefonoField.getText()
+        ));
+        
+        idDoctorField.setText("");
+        nombreDoctorField.setText("");
+        especialidadField.setText("");
+        telefonoField.setText("");   
+    }
     
 }
