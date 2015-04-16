17358ff37b17cd83dd18e5b8092e67f95d859279
pinedas <pinedas@spineda-laptop.ter.teradyne.com>
2015-04-15 21:29:49 -0600

    Se agregan clases Multis

---
+++ src/capaTest/MultiItemResultadoTest.java
@@ -0,0 +1,29 @@
+package capaTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MultiItemResultadoTest {
+
+	@Test
+	public void testCrear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuscarTodos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuscar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBorrar() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/capaTest/MultiResultadoExamenTest.java
+++ src/capaTest/MultiResultadoExamenTest.java
@@ -26,6 +26,10 @@ public class MultiResultadoExamenTest {
         ResultadoExamen expResult = new ResultadoExamen(codigoResultado, nombreLugar, tipoLugar, descripcionLugar, examenAsociado);
         ResultadoExamen result = instance.crear(codigoResultado, nombreLugar, tipoLugar, descripcionLugar, examenAsociado);
         assertEquals(expResult.getDescripcionLugar(), result.getDescripcionLugar());
+        assertEquals(expResult.getCodigoResultado(), result.getCodigoResultado());
+        assertEquals(expResult.getExamenAsociado(), result.getExamenAsociado());
+        assertEquals(expResult.getNombreLugar(), result.getNombreLugar());
+        assertEquals(expResult.getTipoLugar(), result.getTipoLugar());
 	}
 
 	@Test
@@ -35,7 +39,21 @@ public class MultiResultadoExamenTest {
 
 	@Test
 	public void testBuscar() {
-		
+		System.out.println("crear");
+        String codigoResultado = "12345";
+        String nombreLugar = "Granadilla";
+        String tipoLugar = "Feo";
+        String descripcionLugar = "Muchas presas";
+        String examenAsociado = "Sangre";
+        
+        MultiResultadoExamen instance = new MultiResultadoExamen();
+        ResultadoExamen expResult = new ResultadoExamen(codigoResultado, nombreLugar, tipoLugar, descripcionLugar, examenAsociado);
+        ResultadoExamen result = instance.buscar(codigoResultado);
+        assertEquals(expResult.getDescripcionLugar(), result.getDescripcionLugar());
+        assertEquals(expResult.getCodigoResultado(), result.getCodigoResultado());
+        assertEquals(expResult.getExamenAsociado(), result.getExamenAsociado());
+        assertEquals(expResult.getNombreLugar(), result.getNombreLugar());
+        assertEquals(expResult.getTipoLugar(), result.getTipoLugar());
 	}
 
 	@Test
