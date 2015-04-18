ae74e4383c2fb476a00a441b460a3443f21cf656
pinedas <pinedas@spineda-laptop.ter.teradyne.com>
2015-04-18 11:55:56 -0600

    Agregada funcion de crear Consulta

--- src/capaControlador/ControladorConsultas.java
+++ src/capaControlador/ControladorConsultas.java
@@ -3,6 +3,7 @@ package capaControlador;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -63,12 +64,22 @@ public class ControladorConsultas implements Initializable {
     	citas.setAll(lista2);
     	
     	//Iniciar los elementos de creacion de consulta
-    	//ObservableList<String> listaCitas = cbCita.getItems();
-    	//List<String> listaComboCitas = new ArrayList<String>();
-    	//(new MultiCita()).buscarTodos(numeroExpediente)
-		// .forEach(cita -> listaComboCitas.add(cita.getCodigoCita() + "") );
+    	
+    	//Combobox Citas
+    	ObservableList<String> listaDoctores = cbDoctor.getItems();
+    	List<String> listaComboDoctores = new ArrayList<String>();
+    	(new MultiDoctor()).buscarTodos()
+		 .forEach(doctor -> listaComboDoctores.add(doctor.getCedula() + "-" + doctor.getNombre()) );
 
-    	//listaCitas.addAll(listaComboCitas);
+    	listaDoctores.addAll(listaComboDoctores);
+    	
+    	//Combobox doctores
+    	ObservableList<String> listaCitas = cbCita.getItems();
+    	List<String> listaComboCitas = new ArrayList<String>();
+    	(new MultiCita()).buscarTodos(numeroExpediente)
+		 .forEach(cita -> listaComboCitas.add(cita.getCodigoCita() + "") );
+
+    	listaCitas.addAll(listaComboCitas);
     }
     
     public int convertirNumeroConsulta(String csm)
@@ -80,6 +91,20 @@ public class ControladorConsultas implements Initializable {
     @FXML
     protected void agregarConsulta(ActionEvent event) {
         
+        if(cbCita.getValue() == null || cbDoctor.getValue() == null )
+        {
+        	lStatusConsultas.setText("No se puede agregar un consulta sin cita especificada y/o doctor");
+        }else{
+        	 String cedulaDoctor = cbDoctor.getValue().split("-")[0];
+             Date fecha = new Date(dpFechaConsulta.getValue().toEpochDay());
+             String descripcion = tfDescripcion.getText();
+             int expAsociado = convertirNumeroConsulta(cbExpediente.getValue());
+             
+             ObservableList<Consulta> data = tablaConsultas.getItems();
+             Consulta con = (new MultiConsulta()).crear(cedulaDoctor, fecha, descripcion, expAsociado);
+             
+             data.add(con);
+        }
     }
 
 }
